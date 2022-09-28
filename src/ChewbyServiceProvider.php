b9a7542558ece4852ee856baa7cfe92a76b424f5
Chewbathra <Chewbathra@users.noreply.github.com>
2022-09-28 13:35:11 +0000

    Automated action - Apply laravel-pint styling changes

--- src/ChewbyServiceProvider.php
+++ src/ChewbyServiceProvider.php
@@ -29,7 +29,7 @@ class ChewbyServiceProvider extends ServiceProvider
             __DIR__.'/../config/chewby.php' => config_path('chewby.php'),
         ]);
 
-        $this->bootComponents("./resources/views/components");
+        $this->bootComponents('./resources/views/components');
     }
 
     private function bootComponents(string $directory): void
