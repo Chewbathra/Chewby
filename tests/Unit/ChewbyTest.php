47255e443a196e58f0631c6d289726fc0eba7096
Loïc Schupbach <loicschup@gmail.com>
2022-10-02 20:41:48 +0200

    Create chewby tests

--- tests/Unit/ChewbyTest.php
+++ tests/Unit/ChewbyTest.php
@@ -4,19 +4,12 @@ use Chewbathra\Chewby\Facades\Chewby;
 use Illuminate\Support\Facades\Route;
 
 test('generateUrls() should generate index, show and delete URLs for each tracked models', function () {
-    // Route::spy();
-    // Route::shouldReceive("get")
-    //     ->times(4)
-    //     ->e;
-    //     Route::shouldReceive("delete")
-    // ->times(2);
-    // Route::get("/admin", function () {
-    //     echo "salut";
-    // });
-    // Route::get("/admin2", function () {
-    //     echo "salut2";
-    // });
-    // Chewby::generateUrls();
-    // Route::shouldHaveReceived("get")
-    // ->times(2);
+    $routerMock = \Pest\Laravel\partialMock(\Illuminate\Routing\Route::class);
+    Route::shouldReceive('get')
+        ->times(4)
+        ->andReturn($routerMock);
+    Route::shouldReceive('delete')
+        ->times(2)
+        ->andReturn($routerMock);
+    Chewby::generateUrls();
 });
